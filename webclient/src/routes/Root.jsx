e6615ad4b9bbfd64367615a78e25ef7b82240757
Sidney Widmer <hello@sidney.dev>
2025-08-05 15:03:15 +0200

    Remove old webclient (#64)

--- webclient/src/routes/Root.jsx
+++
@@ -1,7 +0,0 @@
-export default function Root() {
-    return (
-        <>
-            <p>Yass Admin</p>
-        </>
-    );
-}
--- webclient/src/store/global.jsx
+++
@@ -1,9 +0,0 @@
-import {create} from "zustand";
-
-const useGameStore = create((set) => ({
-    messages: [],
-    addMessage: (message) => set((state) => ({messages: [...state.messages, message]})),
-    removeMessage: (index) => set((state) => ({messages: state.messages.filter((_, i) => i !== index)})),
-}));
-
-export default useGameStore
