c284b6148de47b763beb31e4e1801301083df5e5
Sidney Widmer <hello@sidney.dev>
2025-10-19 14:19:25 +0200

    Exit game (#76)

--- client/src/store/settings.ts
+++ client/src/store/settings.ts
@@ -14,8 +14,8 @@ interface SettingsState {
 export const useSettingsStore = create<SettingsState>()(
   persist(
     (set) => ({
-      language: SupportedLanguage.DE,
-      cardDeck: CardDeck.SWISS,
+      language: SupportedLanguage.DE as SupportedLanguage,
+      cardDeck: CardDeck.SWISS as CardDeck,
       setLanguage: (lang) => {
         i18n.changeLanguage(lang);
         set({language: lang});
@@ -27,6 +27,13 @@ export const useSettingsStore = create<SettingsState>()(
     {
       name: 'settings-storage',
       storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: (_: SettingsState) => {
+        return (state?: SettingsState | undefined, _?: unknown) => {
+          if (state?.language) {
+            i18n.changeLanguage(state?.language);
+          }
+        }
+      },
     }
   )
 );
